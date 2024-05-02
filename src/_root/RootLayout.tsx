aa6bf34ef18738e95e10a7ef5b966b2478b3566b
Martin Orsag <martin.orsag@websupport.eu>
2024-05-02 15:44:29 +0200

    Handling posts and images.

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,11 +1,22 @@
 import { Outlet } from "react-router-dom";
 
+import Topbar from "@/components/shared/Topbar";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+
 const RootLayout = () => {
   return (
-    <section className="flex flex-1 h-full">
-      <Outlet />
-  </section>
-  )
-}
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
 
-export default RootLayout
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
+  );
+};
+
+export default RootLayout;
